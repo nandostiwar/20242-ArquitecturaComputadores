422fb9b905e739a33a1f21a137671992ce126471
Duvier <duvier.tavera01@unicatolica.edu.co>
2024-09-10 15:13:13 -0500

    Se modifico el espacio de los botones de la Calculadora

--- calculadora-proyecto/front-end-calculadora/src/components/Calculadora.jsx
+++ calculadora-proyecto/front-end-calculadora/src/components/Calculadora.jsx
@@ -27,15 +27,24 @@ function Calculadora(){
         <div className="container">
             <h1 id="txtCalculadora">CALCULADORA</h1>
             <form>
-                <input type="text" className="number" onChange={(e)=>{setNumber1(e.target.value)}}/><br />
-                <input type="text" className="number" onChange={(e)=>{setNumber2(e.target.value)}}/><br />
-                <input type="submit" className="btnEnviar" value="Sumar" onClick={handleSubmit}/>
-                <input type="submit" className="btnEnviar" value="Restar" onClick={handleSubmit}/>
-                <input type="submit" className="btnEnviar" value="Multiplicar" onClick={handleSubmit}/>
-                <input type="submit" className="btnEnviar" value="Mayor" onClick={handleSubmit}/>
-                <input type="submit" className="btnEnviar" value="Menor" onClick={handleSubmit}/>
-                <input type="submit" className="btnEnviar" value="Promedio" onClick={handleSubmit}/>
+                <input type="text" className="number" onChange={(e) => { setNumber1(e.target.value) }} /><br />
+                <input type="text" className="number" onChange={(e) => { setNumber2(e.target.value) }} /><br />
+    
+        <div class="top-buttons">
+            <input type="submit" className="btnEnviar" value="Sumar" onClick={handleSubmit} />
+            <input type="submit" className="btnEnviar" value="Restar" onClick={handleSubmit} />
+            <input type="submit" className="btnEnviar" value="Multiplicar" onClick={handleSubmit} />
+        </div>
+
+        <div class="spacer"></div>
+    
+        <div class="bottom-buttons">
+            <input type="submit" className="btnEnviar" value="Mayor" onClick={handleSubmit} />
+            <input type="submit" className="btnEnviar" value="Menor" onClick={handleSubmit} />
+            <input type="submit" className="btnEnviar" value="Promedio" onClick={handleSubmit} />
+        </div>
             </form>
+
             <Resultado resultado={"El Resultado es :"+ resultado}/>
         </div>
     )
