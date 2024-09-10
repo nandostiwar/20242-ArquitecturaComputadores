3cc232274634c8e7ba2286c05b46d60a608b6f3b
Estudiante <rmanuelorrego@gmail.com>
2024-09-09 20:41:32 -0500

    update

--- calculadora-proyecto/api-calculadora/operaciones/operaciones.js
+++ calculadora-proyecto/api-calculadora/operaciones/operaciones.js
@@ -22,9 +22,29 @@ function multiply(a, b){
     let number2 = parseInt(b);
     return number1 * number2;
 }
+function numMax(a, b){
+    let number1 = parseInt(a);
+    let number2 = parseInt(b);
+    return number1>number2;
+}
+function less(a, b){
+    let number1 = parseInt(a);
+    let number2 = parseInt(b);
+    return number1 < number2;
+}
+
+function prom(a, b){
+    let number1 = parseFloat(a);
+    let number2 = parseFloat(b);
+    return (number1 + number2)/2;
+}
+
 
 module.exports = {
     add,
     subtract,
-    multiply
+    multiply,
+    numMax,
+    less,
+    prom
 }
