25292e94474b82220d01115d70bbf11ae26ebb30
Manolinhopng <rmanuelorrego@gmail.com>
2024-09-10 00:05:46 -0500

    totalmente funcional

--- calculadora-proyecto/api-calculadora/operaciones/operaciones.js
+++ calculadora-proyecto/api-calculadora/operaciones/operaciones.js
@@ -1,50 +1,48 @@
-
 /**
  * Sumar dos cantidades numéricas
- * @param {Number} a 
- * @param {Number} b 
+ * @param {Number} a
+ * @param {Number} b
  * @returns Number
  */
-function add(a, b){
-    let number1 = parseInt(a);
-    let number2 = parseInt(b);
-    return number1 + number2;
+function add(a, b) {
+  let number1 = parseInt(a);
+  let number2 = parseInt(b);
+  return number1 + number2;
 }
 
-function subtract(a, b){
-    let number1 = parseInt(a);
-    let number2 = parseInt(b);
-    return number1 - number2;
+function subtract(a, b) {
+  let number1 = parseInt(a);
+  let number2 = parseInt(b);
+  return number1 - number2;
 }
 
-function multiply(a, b){
-    let number1 = parseInt(a);
-    let number2 = parseInt(b);
-    return number1 * number2;
+function multiply(a, b) {
+  let number1 = parseInt(a);
+  let number2 = parseInt(b);
+  return number1 * number2;
 }
-function numMax(a, b){
-    let number1 = parseInt(a);
-    let number2 = parseInt(b);
-    return number1>number2;
+function numMax(a, b) {
+  let number1 = parseInt(a);
+  let number2 = parseInt(b);
+  return Math.max(number1, number2);
 }
-function less(a, b){
-    let number1 = parseInt(a);
-    let number2 = parseInt(b);
-    return number1 < number2;
-}
-
-function prom(a, b){
-    let number1 = parseFloat(a);
-    let number2 = parseFloat(b);
-    return (number1 + number2)/2;
+function less(a, b) {
+  let number1 = parseInt(a);
+  let number2 = parseInt(b);
+  return Math.min(number1, number2);
 }
 
+function prom(a, b) {
+  let number1 = parseFloat(a);
+  let number2 = parseFloat(b);
+  return (number1 + number2) / 2;
+}
 
 module.exports = {
-    add,
-    subtract,
-    multiply,
-    numMax,
-    less,
-    prom
-}
+  add,
+  subtract,
+  multiply,
+  numMax,
+  less,
+  prom,
+};
