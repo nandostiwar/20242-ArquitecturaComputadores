136e2e4c0c72f12decfd30d168a50add4eee384f
camilo <camilo645@gmail.com>
2024-09-09 20:56:04 -0500

    arregue botones de mayor minimo y promedio

--- calculadora-proyecto/api-calculadora/controllers/calculadoraControllers.js
+++ calculadora-proyecto/api-calculadora/controllers/calculadoraControllers.js
@@ -1,4 +1,4 @@
-const {add, subtract, multiply} = require('../operaciones/operaciones.js');
+const {add, subtract, multiply, numax, numen, prom} = require('../operaciones/operaciones.js');
 
 function sumar(req, res){
     const {body} = req;
@@ -26,9 +26,36 @@ function multiplicar(req, res){
         resultado: result
     })
 }
+function mayor(req, res){
+    const {body} = req;
+    const {number1, number2} = body;
+    const result = numax(number1, number2);
+    res.json({
+        resultado: result
+    })
+}
+function menor(req, res){
+    const {body} = req;
+    const {number1, number2} = body;
+    const result = numen(number1, number2);
+    res.json({
+        resultado: result
+    })
+}
+function promedio(req, res){
+    const {body} = req;
+    const {number1, number2} = body;
+    const result = prom(number1, number2);
+    res.json({
+        resultado: result
+    })
+}
 
 module.exports = {
     sumar,
     restar,
-    multiplicar
+    multiplicar,
+    mayor,
+    menor,
+    promedio
 }
