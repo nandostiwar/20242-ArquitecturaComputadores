64a510a3b8989e90aef46e506f7ccf2bf8d59cbf
kevin <kevinr060799@gmaail.com>
2024-09-11 20:09:16 -0500

    Se cargaron ajustes de mayor, menor y menor

--- calculadora-proyecto/api-calculadora/controllers/calculadoraControllers.js
+++ calculadora-proyecto/api-calculadora/controllers/calculadoraControllers.js
@@ -1,4 +1,4 @@
-const {add, subtract, multiply} = require('../operaciones/operaciones.js');
+const {add, subtract, multiply, may, men, prom} = require('../operaciones/operaciones.js');
 
 function sumar(req, res){
     const {body} = req;
@@ -27,8 +27,38 @@ function multiplicar(req, res){
     })
 }
 
+function mayor(req, res){
+    const {body} = req;
+    const {number1, number2} = body;
+    const result = may(number1, number2);
+    res.json({
+        resultado: result
+    })
+}
+
+function menor(req, res){
+    const {body} = req;
+    const {number1, number2} = body;
+    const result = men(number1, number2);
+    res.json({
+        resultado: result
+    })
+}
+
+function promedio(req, res){
+    const {body} = req;
+    const {number1, number2} = body;
+    const result = prom(number1, number2);
+    res.json({
+        resultado: result
+    })
+}
+
 module.exports = {
     sumar,
     restar,
-    multiplicar
+    multiplicar,
+    mayor,
+    menor,
+    promedio
 }
