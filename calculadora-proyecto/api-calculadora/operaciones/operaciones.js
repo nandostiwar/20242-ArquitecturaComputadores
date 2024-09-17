7721999e5a3eea1107d84f8d480644a03b2c98af
Tatiana <tatianamontoya0428@gmail.com>
2024-09-16 19:16:41 -0500

    Tati modifico botones

--- calculadora-proyecto/api-calculadora/operaciones/operaciones.js
+++ calculadora-proyecto/api-calculadora/operaciones/operaciones.js
@@ -23,8 +23,33 @@ function multiply(a, b){
     return number1 * number2;
 }
 
+function numMax(a,b){
+    let number1 =parseInt(a);
+    let number2 =parseInt(b);
+    if(number1 > number2){
+     return number1
+    } else {return number2
+    }
+ }
+ function numMen(a,b){
+    let number1 =parseInt(a);
+    let number2 =parseInt(b);
+    if(number1 < number2){
+     return number1
+    } else {return number2
+    }
+ }
+ function prom(a,b){
+    let number1 =parseInt(a);
+    let number2 =parseInt(b);
+    return (number1 + number2)/2;
+}
+ 
 module.exports = {
     add,
     subtract,
-    multiply
+    multiply,
+    numMax,
+    numMen,
+    prom
 }
