e7ec8cb921e4841c1efe9027799382d191b507d5
Miguel <lpeepp030@gmail.com>
2024-09-26 21:36:09 -0500

    adelanto

--- calculadora-proyecto/front-end-calculadora/src/components/Calculadora.jsx
+++ calculadora-proyecto/front-end-calculadora/src/components/Calculadora.jsx
@@ -1,46 +1,90 @@
 import { useState } from "react";
-import '../styles/Calculadora.css'
+import "../styles/Calculadora.css";
 import Resultado from "./Resultado";
 
-function Calculadora(){
-    const [number1, setNumber1] = useState('');
-    const [number2, setNumber2] = useState('');
-    const [resultado, setResultado] = useState('');
+//Deficion del estaod de inputs y checkbox
 
-    function handleSubmit(e){
-        e.preventDefault();
-        const operacion = e.target.value;
-        fetch(`http://localhost:3500/v1/calculadora/${operacion}`, {
-            method: 'POST',
-            headers: {"Content-Type": "application/json"},
-            body: JSON.stringify({number1, number2})
-        })
-            .then(res =>res.json())
-            .then(responseData => {
-                setResultado(responseData.resultado)
-                // setResultado(responseData)
-                // console.log(resultado)
-            })
-    }
+function Calculadora() {
+  const [inputs, setInputs] = useState({
+    A: { status: false, value: "" },
+    B: { status: false, value: "" },
+    C: { status: false, value: "" },
+    D: { status: false, value: "" },
+    E: { status: false, value: "" },
+    F: { status: false, value: "" },
+  });
 
-    return (
-        <div className="container">
-            <h1 id="txtCalculadora">CALCULADORA</h1>
-            <form>
-                <input type="checkbox" className="number" onChange={(e)=>{setNumber1(e.target.value)}}/><br />
-                <input type="text" className="number" onChange={(e)=>{setNumber2(e.target.value)}}/><br />
-                <input type="text" className="number" onChange={(e)=>{setNumber2(e.target.value)}}/><br />
-                <input type="text" className="number" onChange={(e)=>{setNumber2(e.target.value)}}/><br />
-                <input type="submit" className="btnEnviar" value="sumar" onClick={handleSubmit}/>
-                <input type="submit" className="btnEnviar" value="restar" onClick={handleSubmit}/>
-                <input type="submit" className="btnEnviar" value="multiplicar" onClick={handleSubmit}/>
-                <input type="submit" className="btnEnviar" value="mayor" onClick={handleSubmit}/>
-                <input type="submit" className="btnEnviar" value="menor" onClick={handleSubmit}/>
-                <input type="submit" className="btnEnviar" value="promedio" onClick={handleSubmit}/>
-            </form>
-            <Resultado resultado={"El resultado es "+ resultado}/>
-        </div>
-    )
+  const [resultadOrden, setResultadOrden] = useState("");
+
+  //manejo de flujo por si cambia el input
+
+  const handleInputChange = (a, key) => {
+    setInputs({
+      ...inputs,
+      [key]: { ...inputs[key], value: a.target.value },
+    });
+  };
+
+  //manejo por si cmabia el checkbox
+
+  const handleCheckboxChange = (a, key) => {
+    setInputs({
+      ...inputs,
+      [key]: { ...inputs[key], status: a.target.checked },
+    });
+  };
+
+  //obteninedo valores de lo habilitaod
+
+  const obtenerValoresStatus = () => {
+    const valores = {};
+    Object.keys(inputs).forEach((key) => {
+      if (inputs[key].status) {
+        valores[key] = parseFloat(inputs[key].value);
+      }
+    });
+  };
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const operacion = e.target.value;
+    fetch(`http://localhost:3500/v1/calculadora/${operacion}`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ number1, number2 }),
+    })
+      .then((res) => res.json())
+      .then((responseData) => {
+        setResultado(responseData.resultado);
+        // setResultado(responseData)
+        // console.log(resultado)
+      });
+  }
+
+  return (
+    <div className="container">
+      <h1>Orden de datos</h1>
+
+      <div className="">
+        {["a", "b", "c", "d", "e", "f"].map((key) => (
+          <div key={key}>
+            <input
+              type="checkbox"
+              onChange={(a) => handleCheckboxChange(a, key)}
+            />
+            <input
+              type="text"
+              disabled={!inputs[key].enable
+                
+              }
+              onChange={(a) => handleInputChange(a, key)}
+              value={inputs[key].value}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default Calculadora
+export default Calculadora;
